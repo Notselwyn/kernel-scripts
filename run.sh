7105a8c7c4a304993a5fd59c1a4013b5268150c4
Your Name <you@example.com>
2023-07-18 19:08:58 -0400

    fix: username in ssh.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ export IMAGE_DISK="$IMAGE/bullseye.img"
 
 qemu-system-x86_64 \
 	-m 1024M \
-	-kernel "$IMAGE/arch/x86/boot/bzImage" \
+	-kernel "$1/arch/x86/boot/bzImage" \
 	-append "console=ttyS0 root=/dev/sda earlyprintk=serial nokaslr net.ifnames=0 kasan_multi_shot=1" \
 	-drive "file=$IMAGE_DISK,format=raw" \
 	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22,hostfwd=tcp:127.0.0.1:445-:445 \
--- run_kmod.sh
+++ run_kmod.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-scp -i bullseye/bullseye.id_rsa -P 10021 -o "StrictHostKeyChecking no" "$1" root@localhost:/tmp/mod.ko && \
-ssh -i bullseye/bullseye.id_rsa -p 10021 -o "StrictHostKeyChecking no" root@localhost "rmmod $2; insmod /tmp/mod.ko"
+export IMAGE="./image"
+export IMAGE_PRIVKEY="$IMAGE/bullseye.id_rsa"
+
+scp -i "$IMAGE_PRIVKEY" -P 10021 -o "StrictHostKeyChecking no" "$1" root@localhost:/tmp/mod.ko && \
+ssh -i "$IMAGE_PRIVKEY" -p 10021 -o "StrictHostKeyChecking no" root@localhost "rmmod $2; insmod /tmp/mod.ko"
 
