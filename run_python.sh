b9582df29f017ec3e273de13561816cea5b93e33
Your Name <you@example.com>
2023-07-18 19:12:41 -0400

    fix: made run_python.sh install python on runtime

--- run_python.sh
+++ run_python.sh
@@ -4,5 +4,4 @@ export IMAGE="./image"
 export IMAGE_PRIVKEY="$IMAGE/bullseye.id_rsa"
 
 scp -i "$IMAGE_PRIVKEY" -P 10021 -o "StrictHostKeyChecking no" "$1" root@localhost:/tmp/script
-ssh -i "$IMAGE_PRIVKEY" -p 10021 -o "StrictHostKeyChecking no" root@localhost 'cd /tmp && chmod +x ./script && ./script'
-
+ssh -i "$IMAGE_PRIVKEY" -p 10021 -o "StrictHostKeyChecking no" root@localhost 'apt update -y && apt install python3 -y && cd /tmp && chmod +x ./script && ./script'
