744400bcef13b04815cde6e6bf5f065c6332d3c6
Your Name <you@example.com>
2023-07-21 17:42:00 -0400

    chore: removed unnecessary port forward

--- run.sh
+++ run.sh
@@ -8,12 +8,10 @@ qemu-system-x86_64 \
 	-kernel "$1/arch/x86/boot/bzImage" \
 	-append "console=ttyS0 root=/dev/sda earlyprintk=serial nokaslr net.ifnames=0 kasan_multi_shot=1" \
 	-drive "file=$IMAGE_DISK,format=raw" \
-	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22,hostfwd=tcp:127.0.0.1:445-:445 \
+	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
 	-net nic,model=e1000 \
 	-enable-kvm \
 	-nographic \
 	-pidfile vm.pid \
 	-gdb tcp::1234 \
 	2>&1 | tee vm.log
-	# -append "console=ttyS0 root=/dev/sda earlyprintk=serial nokaslr net.ifnames=0"
-        #-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22,hostfwd=tcp:127.0.0.1:10445-:445 \
